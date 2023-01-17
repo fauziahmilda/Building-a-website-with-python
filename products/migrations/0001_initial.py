5c72767993e9394d87640beead0359cd725316de
fauziahmilda <fauziah.milda@gmail.com>
2023-01-17 20:25:00 +0700

    add new class for models

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -13,8 +13,9 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='Products',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+            fields=[  # django auto add id, every database table must have id
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
                 ('price', models.FloatField()),
                 ('stock', models.IntegerField()),
--- products/models.py
+++ products/models.py
@@ -8,3 +8,9 @@ class Products(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)  # standar length for url
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
