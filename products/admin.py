b43aa0cdd4a206cc0dee213f047c42feacb77a11
fauziahmilda <fauziah.milda@gmail.com>
2023-01-17 20:45:37 +0700

    registering product class for admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Products
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):  # provide for admin area
+    list_display = ('name', 'price', 'stock')  # column visible in the table
+
+
 # we use admin object, add site, and register to manage this product by admin
 admin.site.register(Products)
