61f9040ae9df0f79bfe27521590315681554c7e2
fauziahmilda <fauziah.milda@gmail.com>
2023-01-17 20:40:06 +0700

    add admin to edit the database

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# manage database by admin
+from .models import Products
 
 # Register your models here.
+# we use admin object, add site, and register to manage this product by admin
+admin.site.register(Products)
--- pyshop/settings.py
+++ pyshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-c)t341q&@t)eqbwzchy4-th%0=#8k2um5uqc$5k@4)t99c*oa@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
