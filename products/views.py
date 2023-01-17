df6549df1e469640e53bfc6b10190e085b3b738e
fauziahmilda <fauziah.milda@gmail.com>
2023-01-17 16:55:17 +0700

    update view file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
+# django package, shortcut is module, render is function
+from django.http import HttpResponse  # for return to the client or browser
 from django.shortcuts import render
 
 # Create your views here.
+
+# /products -> index
+# uniform resource locator
+
+
+def index(request):  # for main page on app
+    # http request
+    return HttpResponse('Hello World')
